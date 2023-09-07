b702bfe436f0d3302883c2dcee379cec8e2a9985
Caleb DEDO <93548924+McDedo@users.noreply.github.com>
2023-09-07 18:22:42 +0100

    Add files via upload
    Prétraiter la vidéo pour en extraire les informations essentielles

---
+++ 1.pretraitement.py
@@ -0,0 +1,28 @@
+from moviepy.video.io.VideoFileClip import VideoFileClip
+import cv2 # Pour la détection des scènes
+
+input_path = input("Entrez le nom du fichier texte : ")
+output_path = "converted.mp4"
+
+video_clip = VideoFileClip(input_path)
+compressed_clip = video_clip
+
+compressed_clip.write_videofile(output_path, codec="libx264", audio_codec="aac")
+
+# Detection des scènes
+
+cap = cv2.VideoCapture(input_path)
+ret, prev_frame = cap.read()
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+    frame_diff = cv2.absdiff(prev_frame, frame)
+    cv2.imshow('Frame Difference', frame_diff)
+    prev_frame = frame
+    if cv2.waitKey(30) & 0xFF == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
