a9bdbe7d816760430b8451dd5d748c532ee8f1d2
Caleb DEDO <93548924+McDedo@users.noreply.github.com>
2023-09-08 17:28:58 +0100

    Update 2.extraire_audio.py

--- 2.extraire_audio.py
+++ 2.extraire_audio.py
@@ -1,19 +1,12 @@
-from moviepy.editor import VideoFileClip
-
-# Chemin vers la vidéo d'entrée
-chemin_video = input("Entrez le nom du fichier texte : ")
-
 # Charger la vidéo
-video = VideoFileClip(chemin_video)
+video = VideoFileClip(output_path)  # Use the converted video as input
+
 
 # Extraire l'audio de la vidéo
 audio = video.audio
 
-# Définir le chemin de sortie pour le fichier audio extrait (format WAV par défaut)
-chemin_audio_extrait = 'humour.wav'
-
-# Enregistrer le fichier audio extrait
-audio.write_audiofile(chemin_audio_extrait)
+output_path = 'extracted_audio.wav'
+audio.write_audiofile(output_path)
 
 video.close()
 audio.close()
