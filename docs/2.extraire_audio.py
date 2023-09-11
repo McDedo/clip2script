1a55abea987c8116830e5961ab1c2bcddc160900
Caleb DEDO <93548924+McDedo@users.noreply.github.com>
2023-09-11 18:30:01 +0100

    Update 2.extraire_audio.py

--- docs/2.extraire_audio.py
+++ docs/2.extraire_audio.py
@@ -1,17 +1,3 @@
-<<<<<<< HEAD:docs/2.extraire_audio.py
-# Charger la vidéo
-video = VideoFileClip(output_path)  # Use the converted video as input
-
-
-# Extraire l'audio de la vidéo
-audio = video.audio
-
-output_path = 'extracted_audio.wav'
-audio.write_audiofile(output_path)
-
-video.close()
-audio.close()
-=======
 import speech_recognition as sr
 
 # Charger la vidéo
@@ -27,4 +13,3 @@ audio.write_audiofile(output_path)
 
 video.close()
 audio.close()
->>>>>>> 271d12ad4d0635a525c9fae3e9d69c994c8476b9:2.extraire_audio.py
