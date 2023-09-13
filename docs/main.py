43059cab5d5669d76c03a10daaf2d46d99d2a847
Caleb DEDO <93548924+McDedo@users.noreply.github.com>
2023-09-13 12:59:14 +0100

    Update main.py

--- docs/main.py
+++ docs/main.py
@@ -58,7 +58,7 @@ def add_punctuation_based_on_pauses(text, pause_threshold=2.0):
             total_pause_duration = 0.0
         else:
             current_sentence.append(word)
-            total_pause_duration += len(word) * 0.04  # Estimation de la durée en secondes par mot (peut nécessiter un ajustement)
+            total_pause_duration += len(word) * 0.04 
             if total_pause_duration >= pause_threshold:
                 sentences.append(' '.join(current_sentence))
                 current_sentence = []
@@ -87,4 +87,88 @@ with sr.AudioFile(audio_file) as source:
         output_file.write(full_text_with_capitalization)
         
         # Partie 4: Génération de PDF
+nltk.download('punkt')
 
+# Fonction pour segmenter un texte en paragraphes et/ou phrases
+def segment_text(text, separator='\n\n', segment_into=('paragraphs', 'sentences')):
+    if not set(segment_into).issubset({'paragraphs', 'sentences'}):
+        raise ValueError("Les éléments de 'segment_into' doivent être 'paragraphs' et/ou 'sentences'.")
+
+    segments = []
+    if 'paragraphs' in segment_into:
+        paragraphs = text.split(separator)
+        segments.extend(paragraphs)
+    
+    if 'sentences' in segment_into:
+        sentences = sent_tokenize(text)
+        segments.extend(sentences)
+    
+    return segments
+
+input_filename = "transcription_complete.txt"
+
+if not os.path.isfile(input_filename):
+    print(f"Le fichier '{input_filename}' n'existe pas.")
+    exit(1)
+
+# Function to detect file encoding
+def detect_encoding(input_filename):
+    with open(input_filename, 'rb') as file:
+        detector = chardet.universaldetector.UniversalDetector()
+        for line in file:
+            detector.feed(line)
+            if detector.done:
+                break
+        detector.close()
+    return detector.result['encoding']
+
+# Determine the file's encoding
+input_encoding = detect_encoding(input_filename)
+
+# Open the file with the detected encoding
+with open(input_filename, 'r', encoding=input_encoding) as file:
+    text = file.read()
+
+# Segmenter le texte en paragraphes et en phrases
+paragraphs = segment_text(text, separator='\n\n', segment_into=['paragraphs'])
+sentences = segment_text(text, segment_into=['sentences'])
+
+formatted_segments = paragraphs + sentences
+output_filename = input("Entrez le nom du fichier de sortie PDF : ")
+if os.path.isfile(output_filename):
+    response = input(f"Le fichier '{output_filename}' existe déjà. Voulez-vous le remplacer ? (O/N) : ").strip().lower()
+    if response != 'o':
+        exit(1)
+font_name = input("Entrez le nom de la police (ou appuyez sur Entrée pour utiliser la police par défaut) : ").strip()
+font_size = input("Entrez la taille de la police (ou appuyez sur Entrée pour utiliser la taille par défaut) : ").strip()
+if not font_name:
+    font_name = 'Helvetica' 
+if not font_size:
+    font_size = 12 
+try:
+    font_size = float(font_size)
+except ValueError:
+    print("La taille de la police doit être un nombre valide.")
+    exit(1)
+
+doc = SimpleDocTemplate(output_filename, pagesize=letter)
+styles = getSampleStyleSheet()
+custom_style = ParagraphStyle(
+    name='CustomStyle',
+    parent=styles['Normal'],
+    fontName=font_name,
+    fontSize=font_size,
+    textColor=colors.black
+)
+segments = []
+root = Tk()
+root.withdraw() 
+output_filename = filedialog.asksaveasfilename(defaultextension=".pdf", filetypes=[("PDF files", "*.pdf")])
+if not output_filename:
+    print("L'emplacement du fichier de sortie n'a pas été spécifié.")
+    exit(1)
+for segment_text in formatted_segments:
+    segment = Paragraph(segment_text, style=custom_style)
+    segments.append(segment)
+doc.build(segments)
+print(f"Le PDF a été créé avec succès sous le nom '{output_filename}'.")
